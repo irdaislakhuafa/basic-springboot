b96be6b719957a6d96f39c00cf34657941aab8d3
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-02-24 01:22:55 +0700

    first commit

---
+++ src/test/java/com/irdaislakhuafa/basicspringboot/BasicSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.basicspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
