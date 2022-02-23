170c85ce206229f34bedfdfb0bf689bd70b907db
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-02-24 01:32:00 +0700

    create application context

--- src/test/java/com/irdaislakhuafa/basicspringboot/BasicSpringbootApplicationTests.java
+++ src/test/java/com/irdaislakhuafa/basicspringboot/BasicSpringbootApplicationTests.java
@@ -1,13 +1,21 @@
 package com.irdaislakhuafa.basicspringboot;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.irdaislakhuafa.basicspringboot.configurations.FirstConfiguration;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class BasicSpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(FirstConfiguration.class);
+		assertNotNull(applicationContext);
 	}
 
 }
