9273aa4aab7fc341d0b1f6d7274f6ee24536e9df
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-13 02:10:32 +0700

    fix duplicate bean with context.getBean("beanName",BeanClassType.class);

--- src/test/java/com/irdaislakhuafa/basicspringboot/bean/DuplictaeBeanTest.java
+++ src/test/java/com/irdaislakhuafa/basicspringboot/bean/DuplictaeBeanTest.java
@@ -1,17 +1,31 @@
 package com.irdaislakhuafa.basicspringboot.bean;
 
 import com.irdaislakhuafa.basicspringboot.configurations.BeansConfigurations;
+import com.irdaislakhuafa.basicspringboot.data.ExampleData;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class DuplictaeBeanTest {
     // test duplicate bean
     @Test
     public void testDuplicateBeans() {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfigurations.class);
         Assertions.assertNotNull(context);
+
+        // user data type and name of bean for select duplicate beans
+        ExampleData data1 = (ExampleData) context.getBean("exampleData1");
+        ExampleData data2 = context.getBean("exampleData1", ExampleData.class);
+
+        // is they same? expected : true
+        Assertions.assertSame(data1, data2);
+
+        data2 = context.getBean("exampleData2", ExampleData.class);
+        Assertions.assertNotSame(data1, data2);
     }
 }
