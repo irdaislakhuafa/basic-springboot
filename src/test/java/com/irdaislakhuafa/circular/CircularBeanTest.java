63959c17003d2526d5668b83a6a579396f1c3c79
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-03-24 01:05:20 +0700

    now i know what is that circular dependency

---
+++ src/test/java/com/irdaislakhuafa/circular/CircularBeanTest.java
@@ -0,0 +1,14 @@
+package com.irdaislakhuafa.circular;
+
+import org.junit.jupiter.api.Test;
+
+public class CircularBeanTest {
+
+    @Test
+    public void testCircular() {
+        // Assertions.assertThrows(Exception.class, () -> {
+        // ApplicationContext context = new
+        // AnnotationConfigApplicationContext(CircularBeanConfiguration.class);
+        // });
+    }
+}
